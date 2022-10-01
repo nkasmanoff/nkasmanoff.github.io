ede3f91b1d07edec8171ae0f34e42bd51eb8f140
nkasmanoff <nkasmanoff@gmail.com>
2022-10-01 14:36:20 -0400

    add: resume

--- src/pages/Experience.js
+++ src/pages/Experience.js
@@ -1,10 +1,13 @@
-import React from "react";
+
+import React, { useState } from 'react';
+import resume from '../images/Noah_Kasmanoff_Resume.pdf'
 
 
 function Experience(){
-
-    return <div>Experience</div>
-
+    return (
+    <div>
+      <object data={resume} className="resume" width="1500" height="3000"></object>      
+    </div>);
 }
 
 export default Experience;
