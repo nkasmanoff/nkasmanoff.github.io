a6ad7f8c84ee7efee05dbc4c482a75ddd9b29795
nkasmanoff <nkasmanoff@gmail.com>
2022-10-16 16:47:07 -0400

    fix for refresh

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <Router basename="nkasmanoff.github.io">
+      <Router basename="">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
