a0c7d46d7a51d519d9f7f750be15b1a307b3eed5
nkasmanoff <nkasmanoff@gmail.com>
2022-10-16 16:40:16 -0400

    router refresh fixed with basename

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import Navbar from "./components/Navbar";
 function App() {
   return (
     <div className="App">
-      <Router>
+      <Router basename="nkasmanoff.github.io">
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
